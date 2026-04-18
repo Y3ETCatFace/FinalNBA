6fcf6e994aa32ef27b3b5541cd38925a0ff54c65
Y3ETCatFace <austinhood0703@icloud.com>
2026-04-18 00:21:39 -0600

    Superb Error Handeling :)

--- src/tools/data_creation.py
+++ src/tools/data_creation.py
@@ -2,7 +2,7 @@ import pandas as pd
 from nba_api.stats.endpoints import LeagueDashPlayerStats
 from nba_api.stats.endpoints import PlayerGameLogs
 from nba_api.stats.endpoints import CommonPlayerInfo #Testing
-from requests.exceptions import ReadTimeout, ConnectTimeout, RequestException
+from requests.exceptions import JSONDecodeError, ReadTimeout, ConnectTimeout, RequestException
 from nba_api.stats.endpoints import PlayByPlayV3
 from nba_api.stats.endpoints import LeagueGameFinder #Ground truth
 from nba_api.stats.endpoints import BoxScoreFourFactorsV3
@@ -15,34 +15,26 @@ import time
 import sys
 import random
 from pathlib import Path
+import requests
 
 #config:
 root = "/Users/austin/Apps/FinalNBA/data/raw/"
 start_year = 2020
 end_year = 2025
 base_delay = 30
-long_pause_every = 100
 long_pause_time = 30
-max_attempts = 3
+max_attempts = 5
 # Assuming your column is named 'player_id'
 
 def master_id_endpoint(game_id, endpoint):
     try:
-        if endpoint == endpoint_map["playbyplay"]:
-            endpoint_class = PlayByPlayV3(
-            game_id=game_id,
-            end_period=10,
-            start_period=1,
-            timeout=50
-            )
-        else:
-            endpoint_class = endpoint_map[endpoint](game_id=game_id, timeout=50)
-    except (ReadTimeout, ConnectTimeout):
-        raise
-    except:
-        print(f"Error fetching data for game ID {game_id} using endpoint {endpoint}.")
-        return None
-    return endpoint_class.get_data_frames()[0]
+        df = endpoint_map[endpoint](game_id=game_id, timeout=50).get_data_frames()[0 if endpoint!="hustle" else 1]
+        return df
+    except AttributeError as e:
+        print(f"Game for {game_id} doesn't exist using endpoint {endpoint}. Creating blank csv")
+        print(e)
+        return pd.DataFrame()
+
 
 def season_range(first, second):
     year_list = []
@@ -53,31 +45,52 @@ def season_range(first, second):
         
     return year_list
 
-def getRandom():
-    year_list = season_range(start_year, end_year)
-    def fourfactors(x):
-        BoxScoreFourFactorsV3(game_id=x, timeout = 15).get_data_frames()[0]
-    def playbyplay(x):
-        PlayByPlayV3(game_id=x, start_period=1, end_period = 10).get_data_frames()[0]
-    def gamelog(x):
-        BoxScoreTraditionalV3(game_id=x, timeout = 15).get_data_frames()[0]
-    def advanced(x):
-        BoxScoreAdvancedV3(game_id=x, timeout = 15).get_data_frames()[0]
-    total_itterations = random.randint(2, 4)
-    for x in range(total_itterations):
-        random_year = random.choice(year_list)
-        df = pd.read_csv(f"{root}{random_year}/gamelog.csv", usecols=["GAME_ID"], dtype={'GAME_ID': str})
-        random_list = df["GAME_ID"].to_list()
-        random_game = random.choice(random_list)
-        random_funcion = random.choice([fourfactors, playbyplay, gamelog, advanced])
+def backoff(data_type, current_id, attempt = 0):
+    while attempt < max_attempts:
+        print(f"Current attempt is {attempt}")
         try:
-            random_funcion(random_game)
-            print(f"{x+1}/{total_itterations} successful for reset api")
-        except:
-            print("failed...waiting 10 seconds for reset api")
-            time.sleep(10)
-            pass
-        time.sleep(1)
+            attempt += 1
+            wait_time = base_delay * (2 ** attempt)
+            print(f"Attempt {attempt} waiting {wait_time}")
+            time.sleep(wait_time)
+            print("Wait complete...\nAttempting to reset api")
+            df = master_id_endpoint(current_id, data_type)
+            attempt = 0
+            break
+        except Exception as e:
+            print(f"{e} Failed retry {attempt}/{max_attempts} ")
+    else:
+        print("api failure system quit")
+
+def getRandom():
+    if random.random() <= 0.5:
+        year_list = season_range(start_year, end_year)
+        def fourfactors(x):
+            BoxScoreFourFactorsV3(game_id=x, timeout = 15).get_data_frames()[0]
+        def playbyplay(x):
+            PlayByPlayV3(game_id=x, start_period=1, end_period = 10).get_data_frames()[0]
+        def gamelog(x):
+            BoxScoreTraditionalV3(game_id=x, timeout = 15).get_data_frames()[0]
+        def advanced(x):
+            BoxScoreAdvancedV3(game_id=x, timeout = 15).get_data_frames()[0]
+        total_itterations = random.randint(2, 4)
+        for x in range(total_itterations):
+            random_year = random.choice(year_list)
+            df = pd.read_csv(f"{root}{random_year}/gamelog.csv", usecols=["GAME_ID"], dtype={'GAME_ID': str})
+            random_list = df["GAME_ID"].to_list()
+            random_game = random.choice(random_list)
+            random_funcion = random.choice([fourfactors, playbyplay, gamelog, advanced])
+            try:
+                random_funcion(random_game)
+                print(f"{x+1}/{total_itterations} successful for reset api")
+            except Exception as e:
+                print(f"failed...waiting 10 seconds for reset api: {e}")
+                time.sleep(10)
+                pass
+            time.sleep(1)
+    else:
+        print("Random sleep")
+        time.sleep(random.uniform(10, 20))
 
 def calculate_percent_completion(num, den):
     return f"{(num/den)*100:.3f}%"
@@ -92,9 +105,13 @@ endpoint_map = {
 }
 
 def creategameIDdata(data_type):
+    long_pause_every = random.randint(70, 90)
     data_root = f"{root}{season}/{data_type}/"
+    attempt = 0
+    request_counter = 1
     game_id_df = pd.read_csv(all_games_path, usecols=["GAME_ID"], dtype={'GAME_ID': str})
     game_list = game_id_df['GAME_ID'].unique().tolist()
+    random.shuffle(game_list)
     game_set = set(game_list)
     if not os.path.exists(data_root):
         Path(data_root).mkdir(exist_ok=True, parents=True)
@@ -102,14 +119,12 @@ def creategameIDdata(data_type):
     if files == game_set:
         print("All " + data_type + " data matchs game_list for season " + season)
     else:
-        random.shuffle(game_list)
         current_game_data = len(os.listdir(data_root))
         total_game_data = len(game_list)
-        attempt = 0
-        request_counter = 1
-        print(f"---------------------\nNow buiding {data_type} data for {season}")
+        print(f"Now buiding {data_type} data for {season}")
         while game_list:
             if request_counter == long_pause_every:
+                long_pause_every = random.randint(70, 90)
                 print("Waiting Safe Delay")
                 time.sleep(long_pause_time)
                 request_counter = 0
@@ -118,39 +133,31 @@ def creategameIDdata(data_type):
             if os.path.exists(data_path):
                 print(f"{data_type} Skipped! {calculate_percent_completion(current_game_data,total_game_data)}")
                 continue
-            request_counter += 1
             try:
+                request_counter += 1
+                if random.random() < 0.02:
+                    getRandom()     
                 df = master_id_endpoint(current_id, data_type)
-                if df is None:
-                    print(f"Endpoint doesn't exist to fetch data for game ID {current_id}. Skipping...")
-                    pd.DataFrame().to_csv(data_path, index=False)  # Save an empty DataFrame to maintain consistency
-                    print(f"{data_type} Skipped! Becuase {current_id} has no endpoint {calculate_percent_completion(current_game_data,total_game_data)}")
-                    continue
                 time.sleep(random.uniform(0.8, 1.6))
+            except (ValueError, JSONDecodeError) as e:
+                print(f"Error decoding JSON response for game ID {current_id} using endpoint {data_type}.")
+                reset_http()
+                backoff(data_type, current_id)
+            
             except (ReadTimeout, ConnectTimeout) as e:
                 print(f"{e} Error fetching {data_type} data for game ID {current_id}. Attempting to reset API and retry...")
-                while attempt < max_attempts:
-                    try:
-                        attempt += 1
-                        wait_time = base_delay * (2 ** attempt)
-                        print(f"Attempt {attempt} waiting {wait_time}")
-                        time.sleep(wait_time)
-                        print("Wait complete...\nAttempting to reset api")
-                        try:
-                            getRandom()
-                        except Exception as e:
-                            print(f"{e} Reset failed")
-                        df = master_id_endpoint(current_id, data_type)
-                        attempt = 0
-                        break
-                    except Exception as e:
-                        print(f"{e} Failed retry {attempt}/{max_attempts} ")
-                else:
-                    print("api failure system quit")
-                    sys.exit(1)
+                reset_http()
+                backoff(data_type, current_id)
+                
             df.to_csv(data_path, index=False)
             current_game_data += 1
             print(f"Saved! {calculate_percent_completion(current_game_data,total_game_data)} complete!")
+
+def reset_http():
+    print("Savoir Reset")
+    for adapter in requests.sessions.Session().adapters.values():
+        adapter.close()
+
 if __name__ == "__main__":
     for season in season_range(start_year, end_year): #Verifys players in season
         skeleton_save_path = f"{root}{season}/active_players.csv"
@@ -205,7 +212,7 @@ if __name__ == "__main__":
         
         creategameIDdata("advanced")
         creategameIDdata("defensive")
-        x = 1
+    
         creategameIDdata("fourfactors")
         creategameIDdata("hustle")
 
